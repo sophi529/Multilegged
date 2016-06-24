3ccb8214e3befddd2fefeb16a627d31f1f98e9f5
Sophie Dewil <Sophi529@156-56-91-235.dhcp-bl.indiana.edu>
2016-06-24 11:58:09 -0400

    17

--- Multilegged/Tsearch.cpp
+++ Multilegged/Tsearch.cpp
@@ -449,7 +449,7 @@ void TSearch::UpdatePopulationStatistics(void)
 void TSearch::DisplayPopulationStatistics(void)
 {
     ofstream xlev;
-    xlev.open ("/Users/Sophi529/Desktop/Multilegged/testing/sixlegtest/17.dat", ios::app);
+    xlev.open ("/Users/Sophi529/Desktop/Multilegged/testing/sixlegtest/18.dat", ios::app);
     if (PopulationStatisticsDisplayFunction != NULL)
         (*PopulationStatisticsDisplayFunction)(Gen,BestPerf,AvgPerf,PerfVar);
     else {
